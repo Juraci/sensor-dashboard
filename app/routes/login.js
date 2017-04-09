84daad2038f45ce532439538f13324c590ec5e5b
Juraci Vieira <juraci.vieira@gmail.com>
2017-04-09 10:47:02 -0300

    form submit on enter

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,40 +1,41 @@
-  import Ember from 'ember';
+import Ember from 'ember';
 
-  export default Ember.Route.extend({
-    authenticator: Ember.inject.service(),
-    sessionManager: Ember.inject.service(),
-    notify: Ember.inject.service(),
+const { inject } = Ember;
 
-    actions: {
-      didTransition() {
-        this.controller.set('spinner', false);
-      },
+export default Ember.Route.extend({
+  authenticator: inject.service(),
+  sessionManager: inject.service(),
+  notify: inject.service(),
 
-      onEmailChange(value) {
-        this.controller.set('email', value);
-      },
+  actions: {
+    didTransition() {
+      this.controller.set('spinner', false);
+    },
 
-      onPasswordChange(value) {
-        this.controller.set('password', value);
-      },
+    onEmailChange(value) {
+      this.controller.set('email', value);
+    },
 
-      login() {
-        this.controller.set('spinner', true);
-        const email = this.controller.get('email');
-        const password = this.controller.get('password');
-        this.get('authenticator').authenticate(email, password)
-          .then((result) => {
-            this.controller.set('spinner', false);
-            Ember.Logger.warn(result);
-            if(result.success) {
-              this.get('sessionManager').setToken(result.token);
-              this.transitionTo('dashboard');
-            } else {
-              this.get('notify').alert('Incorrect email or password', {
-                classNames: ['alert-notification']
-              });
-            }
+    onPasswordChange(value) {
+      this.controller.set('password', value);
+    },
+
+    login() {
+      this.controller.set('spinner', true);
+      const email = this.controller.get('email');
+      const password = this.controller.get('password');
+      this.get('authenticator').authenticate(email, password)
+        .then((result) => {
+          this.controller.set('spinner', false);
+          if(result.success) {
+            this.get('sessionManager').setToken(result.token);
+            this.transitionTo('dashboard');
+          } else {
+            this.get('notify').alert('Incorrect email or password', {
+              classNames: ['alert-notification']
+            });
+          }
         });
     },
-  }
+  },
 });
