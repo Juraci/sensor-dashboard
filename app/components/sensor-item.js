e85668ffde5e0e98277d5fad35776425214de0bb
Juraci Vieira <juraci.vieira@gmail.com>
2017-04-13 10:21:01 -0300

    using instance property instead class property

--- app/components/sensor-item.js
+++ app/components/sensor-item.js
@@ -4,13 +4,14 @@ import Config from 'sensor-dashboard/config/environment';
 const { computed } = Ember;
 
 export default Ember.Component.extend({
-  messages: [],
+  messages: null,
   motionAmount: computed('messages.length', function() {
     return this.get('messages.length');
   }),
 
   init() {
     this._super(...arguments);
+    this.set('messages', []);
     const source = new EventSource(`${Config.APP.sse}/cards/${this.get('sensor.boardId')}/stream`);
     source.onmessage =  (e) => {
       if(e.data !== 'sse ready') {
