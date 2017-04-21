69d4384ab5e546194d315efa7c208edc80a5fbea
Juraci Vieira <juraci.vieira@gmail.com>
2017-04-21 19:27:16 -0300

    less logging for production

--- config/environment.js
+++ config/environment.js
@@ -51,11 +51,6 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-     ENV.APP.LOG_RESOLVER = true;
-     ENV.APP.LOG_ACTIVE_GENERATION = true;
-     ENV.APP.LOG_TRANSITIONS = true;
-     ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-     ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.APP.sse = 'https://nodemcu-listener.herokuapp.com';
     ENV.APP.sensorsManagement = 'https://sensors-management.herokuapp.com';
   }
