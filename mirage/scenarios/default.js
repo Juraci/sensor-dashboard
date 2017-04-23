71c9225df5b52675eded82174e5e5053788bf446
Juraci Vieira <juraci.vieira@gmail.com>
2017-04-22 23:38:27 -0300

    improving the alerts side color

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -3,6 +3,7 @@ export default function(server) {
   const sensor1 = server.create('sensor', { description: 'living room', boardId: '109812JMA', user });
   server.create('sensor', { description: 'Bedroom', boardId: '109812JMZ', user });
   server.create('alert', { message: 'Motion 10/02/1987 10:29:19 pm', seen: false, sensorId: sensor1.id });
+  server.create('alert', { message: 'Motion 10/02/1987 10:30:19 pm', seen: false, sensorId: sensor1.id });
 
   /*
     Seed your development database using your factories.
