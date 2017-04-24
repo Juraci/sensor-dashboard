26b51f9345106cf6a2ce0fb4ce2be3c83c7e7787
Juraci Vieira <juraci.vieira@gmail.com>
2017-04-23 21:50:32 -0300

    updaing default scenarios

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -2,8 +2,18 @@ export default function(server) {
   const user = server.create('user', { email: 'honestly@haremscarem.com', password: 'pass1234' });
   const sensor1 = server.create('sensor', { description: 'living room', boardId: '109812JMA', user });
   server.create('sensor', { description: 'Bedroom', boardId: '109812JMZ', user });
-  server.create('alert', { message: 'Motion 10/02/1987 10:29:19 pm', seen: false, sensorId: sensor1.id });
-  server.create('alert', { message: 'Motion 10/02/1987 10:30:19 pm', seen: false, sensorId: sensor1.id });
+  server.create('alert', {
+    message: 'Motion 10/02/1987 10:29:23 pm',
+    seen: false,
+    sensorId: sensor1.id,
+    createdAt: '1987-04-23T22:29:23.563Z'
+  });
+  server.create('alert', {
+    message: 'Motion 10/02/1987 10:30:19 pm',
+    seen: false,
+    sensorId: sensor1.id,
+    createdAt: '1987-04-23T22:30:19.563Z'
+  });
 
   /*
     Seed your development database using your factories.
